59eaabeca07404b44e8ea7aa6c60368fe021f69f
Kirill Petrosian <kiril.petrosian@wsc-sports.com>
2022-08-28 16:14:30 +0400

    money flows controller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import initializeDB from "./utils/initializeDB.js";
 
 import authRouter from "./routes/auth.route.js";
+import moneyFlowRouter from "./routes/money-flow.router.js";
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use("/", (req, res, next) => {
 app.use("/", bodyParser.json({ limit: "50mb" }));
 
 app.use("/auth", authRouter);
+app.use("/moneyFlow", moneyFlowRouter);
 
 app.use("/", async (err, req, res, next) => {
   await res.status(err.status || 500).json({
--- utils/initializeDB.js
+++ utils/initializeDB.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import Category from "../models/category.model.js";
+import Currency from "../models/currency.model.js";
 import { encrypt } from "../utils/password.js";
 
 export default async function () {
@@ -8,4 +10,67 @@ export default async function () {
     login: "materuilist",
     password: await encrypt("borow123"),
   });
+
+  await Category.create([
+    {
+      name: "Bills",
+    },
+    {
+      name: "Car",
+    },
+    {
+      name: "Clothes",
+    },
+    {
+      name: "Communications",
+    },
+    {
+      name: "Eating out",
+    },
+    {
+      name: "Entertainment",
+    },
+    {
+      name: "Food",
+    },
+    {
+      name: "Gifts",
+    },
+    {
+      name: "Health",
+    },
+    {
+      name: "House",
+    },
+    {
+      name: "Pets",
+    },
+    {
+      name: "Sports",
+    },
+    {
+      name: "Taxi",
+    },
+    {
+      name: "Toiletry",
+    },
+    {
+      name: "Transport",
+    },
+  ]);
+
+  await Currency.create([
+    {
+      name: "Dollar",
+      symbol: "$",
+    },
+    {
+      name: "Dram",
+      symbol: "֏",
+    },
+    {
+      name: "Ruble",
+      symbol: "₽",
+    },
+  ]);
 }
